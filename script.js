074ea3287afb3ca87e5ac4871b1d0f90c1ab87de
Mahfuz <mahfuzru5634@gmail.com>
2025-09-04 09:17:42 +0600

    working on modal section

--- script.js
+++ script.js
@@ -4,52 +4,72 @@ const loadLessson = () => {
     .then((json) => displayData(json.data));
 };
 
+// loadworddetail
+
+const worddetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res=await fetch(url);
+  const details=await res.json();
+  displayworddetails(details.data)
+};
+
+const displayworddetails = (word)=>{
+    const modaldata=document.getElementById('modal-container');
+    modaldata.innerText='On the procees';
+    document.getElementById('my_modal_5').showModal();
+}
+
+
 const wordload = (no) => {
   const url = `https://openapi.programming-hero.com/api/level/${no}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) =>{
-      const allbuttonclass=document.getElementsByClassName('btn-class');
-      for(const it of allbuttonclass){
-        it.classList.remove('btn-design');
+    .then((data) => {
+      const allbuttonclass = document.getElementsByClassName("btn-class");
+      for (const it of allbuttonclass) {
+        it.classList.remove("btn-design");
       }
 
-      const allbutton=document.getElementById(`btn-id-${no}`);
-      allbutton.classList.add('btn-design');
-      loadwordData(data.data)});
+      const allbutton = document.getElementById(`btn-id-${no}`);
+      allbutton.classList.add("btn-design");
+      loadwordData(data.data);
+    });
 };
 
 const loadwordData = (data) => {
-   const wordContainer=document.getElementById('word-container-section');
-   wordContainer.innerHTML='';
-    
-   if(data.length==0){
-    wordContainer.innerHTML=`
+  const wordContainer = document.getElementById("word-container-section");
+  wordContainer.innerHTML = "";
+
+  if (data.length == 0) {
+    wordContainer.innerHTML = `
     <div class="col-span-full text-center p-10">
             <img class="mx-auto" src="./assets/alert-error.png" alt="">
             <h1 class="opacity-80 text-1xl font-bangla mb-4">এই Lesson এ এখনও শব্দ যুক্ত করা হয়নি  </h1>
             <h1 class="text-3xl font-bangla font-bold">অন্য একটি Lesson Select করুন।</h1>
         </div>
     `;
-   }
+  }
 
-   for(const dt of data){
-    const newword=document.createElement('div');
-    newword.innerHTML=`
+  for (const dt of data) {
+    const newword = document.createElement("div");
+    newword.innerHTML = `
     <div class="text-center bg-white p-10 h-full shadow-indigo-100 rounded-md">
-        <h1 class="text-2xl font-bold m-3">${dt.word?dt.word:"🚫"}</h1>
+        <h1 class="text-2xl font-bold m-3">${dt.word ? dt.word : "🚫"}</h1>
         <h4 class="font-semibold font-bangla m-3">Meaning /Pronounciation</h4>
-        <h1 class="text-1xl m-3">"${dt.meaning?dt.meaning:"🚫"
-        }/ ${dt.pronunciation?dt.pronunciation:"🚫"}"</h1>
+        <h1 class="text-1xl m-3">"${dt.meaning ? dt.meaning : "🚫"}/ ${
+      dt.pronunciation ? dt.pronunciation : "🚫"
+    }"</h1>
 
         <div class="flex justify-between items-center mt-10">
-            <button class="btn bg-[#e8f4ff] h-[56px] w-[56px] rounded-md flex justify-center items-center"><i class="fa-solid fa-circle-info"></i></button>
+            <button onclick="worddetails(${
+              dt.id
+            })" class="btn bg-[#e8f4ff] h-[56px] w-[56px] rounded-md flex justify-center items-center"><i class="fa-solid fa-circle-info"></i></button>
            <button class="btn bg-[#e8f4ff] h-[56px] w-[56px] rounded-md flex justify-center  items-center"> <i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
-    `
+    `;
     wordContainer.appendChild(newword);
-   }
+  }
 };
 
 const displayData = (data) => {
